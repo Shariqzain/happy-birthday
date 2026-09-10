f6148f5d5d7e89dd8ff72c5af8f31077d2014f0d
Shariqzain <shariqzain06@gmail.com>
2026-09-11 03:54:04 +0530

    Use active Google Sheets deployment

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import BirthdayPage from './BirthdayPage';
 import Letter from './letter';
 
 const CYAN_BALLOON_COLORS = ['#00e5ff', '#00bcd4', '#26c6da', '#00acc1', '#4dd0e1', '#80deea'];
-const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyWt3NAFRBWh4N7GvDLIGH3Rbwed6IqER-sDPAp6K2fkN_Y5LgIr_sS0H8onOMiJhU6/exec';
+const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyR-KXV6lO2dTcXiATyOizepP0YNSp4ksVafaDzqcYMvzmwZ7ZdTD865tXPpAMz5nkZ/exec';
 
 function Typewriter({ text, start, speed = 32, onComplete }) {
   const [visibleText, setVisibleText] = useState('');
